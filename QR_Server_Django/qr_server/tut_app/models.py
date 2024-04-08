1c8db97113fd26bd3cc7f9aae36adf43137c524b
unknown <www_123_val@abv.bg>
2024-04-08 05:24:52 +0200

    Add Ads model

--- QR_Server_Django/qr_server/tut_app/models.py
+++ QR_Server_Django/qr_server/tut_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from datetime import date
 
 # Create your models here.
+class Ads(models.Model):
+    store_name = models.CharField(max_length=200)
+    description = models.TextField(max_length=1000)
+    location = models.CharField(max_length=500)
+    num_limit = models.IntegerField()
+    date_limit = models.DateField(default=date.today())
